52aeb604afb572e69d95516fceabdf04b4913652
apfitch <alan.fitch@ericsson.com>
2018-01-18 16:50:16 +0000

    Very simple playout

--- playout_test/playout_test.cpp
+++ playout_test/playout_test.cpp
@@ -3,12 +3,13 @@
 #include <string>
 #include <chrono>
 #include <thread>
+#include <deque>
 
-void pause_us(uint32_t us)
+void pause_ns(const uint32_t & ns)
 {
     using namespace std::this_thread;
     using namespace std::chrono;
-    sleep_for(microseconds(us));
+    sleep_for(nanoseconds(ns));
 }
 
 int main ( int argc, char ** argv)
@@ -19,6 +20,14 @@ int main ( int argc, char ** argv)
     using std::ifstream;
     using std::string;
     using std::ios;
+    using std::deque;
+
+    deque<char> qbuff;
+
+    // target bitrate in Mbps, b = bit!
+    uint32_t target_bitrate_Mbps = 1;
+    uint32_t buff_size = 1024; // bytes = 8192 bits
+    uint32_t buff_size_bits = buff_size * 8;
 
     ifstream infile;
     ofstream outfile;
@@ -32,14 +41,42 @@ int main ( int argc, char ** argv)
     outfile.open(ofname, ios::binary);
 
     cout << "Copying input to output" << endl;
-
     char c;
+    uint32_t i = 0;
+    auto start = std::chrono::high_resolution_clock::now();
+    auto end = start;
+    uint32_t buff_level_bits = 0;
+
     while (infile.get(c))
     {
         if (infile.eof()) {
             break;
         }
-        outfile << c;
+
+        // push the input into a queue
+        qbuff.push_front(c);
+        buff_level_bits += 8;
+
+        if (buff_level_bits > buff_size_bits)
+        {
+            for (unsigned int i = 0; i< buff_level_bits/8; i++)
+            {
+                pause_ns(1); // The minimum delay seems to be about 60us!
+
+                c = qbuff.back();
+                qbuff.pop_back();
+                outfile << c;
+            }
+            buff_level_bits = 0;
+        }
+
+//        start = std::chrono::high_resolution_clock::now();
+
+        //
+//      nanosleep(&delay, NULL);    // minimum delay also about 60us
+//        end = std::chrono::high_resolution_clock::now();
+//        std::chrono::duration<double, std::micro> elapsed = end-start;
+//        std::cout << "Waited " << elapsed.count() << " us\n";
     }
 
     infile.close();
