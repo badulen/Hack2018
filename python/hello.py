2ed6fed554d1ae8421ff5ce02887513259d3d8c7
apfitch <alan.fitch@ericsson.com>
2018-01-19 13:48:28 +0000

    Updated python to tail from a file

--- python/hello.py
+++ python/hello.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from random import *
 import threading
+import tailer
 
 app = Flask(__name__)
 count = 0
+filename = 'file1.txt'
 
 def get_data():
     global count
+    global filename
     threading.Timer(1.0, get_data).start()
-    count = 10*random();
+    count = tailer.tail(open(filename),1)[0]
+#    count = 10*random();
     print 'New count' + str(count)
 
 get_data()
